15d7957afdd78b4c251e179619d6031a3ad2408b
Koushik Chakraborty <ckoushikp@gmail.com>
2016-11-13 17:54:48 +0530

    Update MineSweeper.java

--- MineSweeper/MineSweeper.java
+++ MineSweeper/MineSweeper.java
@@ -8,24 +8,24 @@ import javax.swing.*;
 public class MineSweeper extends JPanel {
 	
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Drawing by Koushik");
+	    JFrame frame = new JFrame("Drawing by Koushik");
 	    frame.setSize(600,600);
 	    frame.setLocationRelativeTo(null);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
-        Container pane = frame.getContentPane();
-        pane.setLayout(new GridLayout(9,9));
+            Container pane = frame.getContentPane();
+            pane.setLayout(new GridLayout(9,9));
 	    pane.setBackground(Color.CYAN);
 	    
         //add all grids
 	    Mine mine[][] = new Mine[9][9];
-        for(int i=0;i<9;i++) {
+            for(int i=0;i<9;i++) {
         	for(int j=0; j<9; j++) {
             	mine[i][j] = new Mine();
             	pane.add(mine[i][j]);
             	
         	}
-        }
+            }
         //Implant the bombs
 	    for(int i=0; i <10; i++) {
 		    int temp = (int)(Math.random()*81);
